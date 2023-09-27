568b95e06c75aee04a7c87d32b22ea72e92a15eb
ccoliu <52516787+ccoliu@users.noreply.github.com>
2023-09-27 15:56:30 +0800

    Update B11001035.cpp

--- HW2/B11001035.cpp
+++ HW2/B11001035.cpp
@@ -7,6 +7,29 @@
 using namespace std;
 
 int var = 0;
+vector<string> varlab;
+string func_label = "";
+
+void output_file(ofstream &fout, vector<string> vec)
+{
+    fout << ".i " << var << endl;
+    fout << ".o 1" << endl;
+    fout << ".ilb ";
+    for (int i=0;i<varlab.size();i++)
+    {
+        if (i != varlab.size()-1) fout << varlab[i] << " ";
+        else fout << varlab[i] << endl;
+    }
+    fout << ".ob " << func_label << endl;
+    fout << ".p " << vec.size() << endl;
+    for (int i=0;i<vec.size();i++)
+    {
+        fout << vec[i] << " 1" << endl;
+    }
+    fout << ".e" << endl;
+    exit(0);
+    return;
+}
 
 vector<vector<string>> make_group(vector<string> vec)
 {
@@ -25,7 +48,7 @@ vector<vector<string>> make_group(vector<string> vec)
 }
 
 template<class T>
-void findrepeat(vector<T> &vec)
+void findrepeat(T &vec)
 {
     sort(vec.begin(), vec.end());
     for (int i=0;i<vec.size()-1;i++)
@@ -37,6 +60,32 @@ void findrepeat(vector<T> &vec)
         }
     }
     return;
+}
+
+vector<string> petrick_method(vector<vector<string>> &vec)
+{
+    if (vec.size() > 1)
+    {
+        vector<string> tempvec;
+        for (int i=0;i<vec[0].size();i++)
+        {
+            for (int j=0;j<vec[1].size();j++)
+            {
+                string tempstr = "";
+                tempstr += vec[0][i];
+                tempstr += vec[1][j];
+                tempvec.push_back(tempstr);
+            }
+        }
+        findrepeat(tempvec);
+        vec.erase(vec.begin()+1);
+        vec[0] = tempvec;
+        return petrick_method(vec);
+    }
+    else
+    {
+        return vec[0];
+    }
 }
 
 int string_to_int(string s)
@@ -89,8 +138,6 @@ int main(int argc, char* argv[])
         string str = "";
         int out_var = 0;
 
-        vector<string> varlab;
-        string func_label = "";
         int statement_var = 0;
         vector<vector<string>> statement;
         vector<string> expr;
@@ -300,7 +347,10 @@ int main(int argc, char* argv[])
             group = make_group(prime_implicant[literal]);
             literal--;
         }
+
         findrepeat(prime_implicant[literal+1]);
+        vector<string> essential_prime_implicant;
+
         for (int i=var;i>=0;i--)
         {
             if (prime_implicant[i].empty()) break;
@@ -308,11 +358,152 @@ int main(int argc, char* argv[])
             for (int j=0;j<prime_implicant[i].size();j++)
             {
                 cout << prime_implicant[i][j] << "(" << prime_implicant_bool[i][j] << ") ";
+                if (prime_implicant_bool[i][j] == false)
+                {
+                    essential_prime_implicant.push_back(prime_implicant[i][j]);
+                }
             }
             cout << endl;
         }
         //find essential prime implicant
         
+        vector<vector<string>> essential_prime_implicant_int(pow(2,var));
+        vector<string> candidates;
+
+        for (int i=0;i<essential_prime_implicant.size();i++)
+        {
+            vector<string> temp_expr;
+            temp_expr.push_back(essential_prime_implicant[i]);
+            temp_expr = get_all_expr(temp_expr);
+            for (int j=0;j<temp_expr.size();j++)
+            {
+                int tempint = string_to_int(temp_expr[j]);
+                essential_prime_implicant_int[tempint].push_back(essential_prime_implicant[i]);
+            }
+        }
+
+        for (int i=0;i<essential_prime_implicant_int.size();i++)
+        {
+            if (essential_prime_implicant_int[i].empty() || find(minterms.begin(),minterms.end(),i) == minterms.end()) continue;
+            if (essential_prime_implicant_int[i].size() == 1)
+            {
+                candidates.push_back(essential_prime_implicant_int[i][0]);
+                for (int j=0;j<essential_prime_implicant_int.size();j++)
+                {
+                    if (!essential_prime_implicant_int[j].empty())
+                    {
+                        if (find(essential_prime_implicant_int[j].begin(), essential_prime_implicant_int[j].end(), essential_prime_implicant_int[i][0]) != essential_prime_implicant_int[j].end())
+                        {
+                            essential_prime_implicant_int[j].clear();
+                            vector<int>::iterator it = find(minterms.begin(), minterms.end(), j);
+                            if (it != minterms.end())
+                            {
+                                minterms.erase(it);
+                            }
+                        }
+                    }
+                }
+                i = 0;
+            }
+        }
+
+        cout << "essential prime implicants: ";
+        for (int i=0;i<candidates.size();i++)
+        {
+            cout << candidates[i] << " ";
+        }
+        cout << endl;
+
+        //if all candidates are found, output the result, else, do petrick's method
+        if (minterms.empty()) output_file(fout, candidates);
+
+        //petrick's method
+        vector<string> symbol;
+        vector<string> left_candidates;
+        vector<vector<string>> petrick(pow(2,var));
+        for (int i=0;i<minterms.size();i++)
+        {
+            for (int j=0;j<essential_prime_implicant_int[minterms[i]].size();j++)
+            {
+                if (find(left_candidates.begin(), left_candidates.end(), essential_prime_implicant_int[minterms[i]][j]) == left_candidates.end())
+                {
+                    left_candidates.push_back(essential_prime_implicant_int[minterms[i]][j]);
+                }
+            }
+        }
+
+        for (int i=0;i<left_candidates.size();i++)
+        {
+            string temp = "";
+            temp.push_back('A'+i);
+            symbol.push_back(temp);
+            cout << symbol[i] << ": " << left_candidates[i] << endl;
+        }
+
+        for (int i=0;i<minterms.size();i++)
+        {
+            for (int j=0;j<essential_prime_implicant_int[minterms[i]].size();j++)
+            {
+                for (int k=0;k<left_candidates.size();k++)
+                {
+                    if (essential_prime_implicant_int[minterms[i]][j] == left_candidates[k] && find(petrick[minterms[i]].begin(), petrick[minterms[i]].end(), symbol[k]) == petrick[minterms[i]].end())
+                    {
+                        petrick[minterms[i]].push_back(symbol[k]);
+                    }
+                }
+            }
+        }
+
+        for (int i=0;i<petrick.size();i++)
+        {
+            if (petrick[i].empty()) continue;
+            for(int j=0;j<petrick[i].size();j++)
+            {
+                if (j != petrick[i].size()-1) cout << petrick[i][j] << "+";
+                else cout << petrick[i][j];
+            }
+            cout << endl;
+        }
+
+        vector<vector<string>> petrick_copy;
+        for (int i=0;i<petrick.size();i++)
+        {
+            if (!petrick[i].empty())
+            {
+                petrick_copy.push_back(petrick[i]);
+            }
+        }
+
+        vector<string> petrick_result = petrick_method(petrick_copy);
+        for (int i=0;i<petrick_result.size();i++)
+        {
+            findrepeat(petrick_result[i]);
+        }
+        findrepeat(petrick_result);
+
+        string result = "";
+        cout << "petrick result:" << endl;
+        for (int i=0;i<petrick_result.size();i++)
+        {
+            if (i != petrick_result.size()-1) cout << petrick_result[i] << "+";
+            else cout << petrick_result[i] << endl;
+            if (result == "")
+            {
+                result = petrick_result[i];
+            }
+            else
+            {
+                if (petrick_result[i].length() < result.length())
+                {
+                    result = petrick_result[i];
+                }
+            }
+        }
+        for (int i=0;i<result.length();i++)
+        {
+            candidates.push_back(left_candidates[result[i]-'A']);
+        }
+        output_file(fout, candidates);
     }
     else
     {
